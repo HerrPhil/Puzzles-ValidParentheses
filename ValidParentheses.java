88cecf6954087e4b59fcdf4be9cdec8f6b590c84
HerrPhil <phillip.j.wray.esq@gmail.com>
2024-08-12 15:32:33 -0600

    Write the code to check the valid parentheses

---
+++ ValidParentheses.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.*;
+import java.math.BigInteger;
+
+public class ValidParentheses {
+
+    public static void main(String [] args) {
+        System.out.printf("Hello Valid Parentheses Solution%n");
+        if (args != null && args.length == 1 && args[0].toLowerCase().equals("-usage")) {
+            System.out.printf("java ValidParentheses%n");
+            return;
+        }
+
+//        String input = "(){}";
+//        String input = "(){}(";
+//        String input = "[(){}]";
+//        String input = "[{(()){}}]";
+//        String input = "[(])";
+        String input = "{[1,2,3](a,b,c),?}";
+        
+        ValidParentheses validParentheses = new ValidParentheses(input);
+
+        boolean result = validParentheses.solution();
+
+        if (result) {
+
+            System.out.printf("the parentheses %s are valid%n", input);
+
+        } else {
+
+            System.out.printf("the parentheses %s are invalid%n", input);
+
+        }
+
+    }
+
+    private String input;
+
+    public ValidParentheses(String input) {
+
+        this.input = input;
+
+    }
+
+    public boolean solution() {
+
+        // Initialize a stack
+        Stack<Character> stack = new Stack<>();
+
+        char [] parentheses = input.toCharArray();
+
+        for (int i = 0; i < parentheses.length; i++) {
+
+            char nextParenthesis = parentheses[i];
+
+            boolean isOpen = nextParenthesis == '(' || nextParenthesis == '[' || nextParenthesis == '{';
+
+            if (isOpen) {
+
+                stack.push(Character.valueOf(nextParenthesis));
+
+            }
+
+            boolean isClose = nextParenthesis == ')' || nextParenthesis == ']' || nextParenthesis == '}';
+
+            if (isClose) {
+
+                // figure out if this has a matching open bracket
+                boolean isPoppable =
+                    stack.peek().equals(Character.valueOf('(' )) && nextParenthesis == ')' ||
+                    stack.peek().equals(Character.valueOf('[' )) && nextParenthesis == ']' ||
+                    stack.peek().equals(Character.valueOf('{' )) && nextParenthesis == '}';
+
+                if (isPoppable) {
+
+                    stack.pop();
+
+                }
+            }
+
+        }
+
+        return stack.empty();
+    }
+
+}
